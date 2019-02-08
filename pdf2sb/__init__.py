94686e466f9f8ca1eccdff9217184194e3d15edf
reiyw <reiyw.setuve@gmail.com>
2019-02-08 19:10:26 +0900

    `--page` option

--- pdf2sb/__init__.py
+++ pdf2sb/__init__.py
@@ -1,6 +1,7 @@
 import tempfile
+from itertools import chain
 from pathlib import Path
-from typing import List
+from typing import List, Iterator, Optional
 
 import click
 import gyazo
@@ -9,6 +10,27 @@ from pdf2image import convert_from_path
 from tqdm import tqdm
 
 __version__ = "0.2.0"
+
+
+def parse_range(expr: str) -> Iterator[int]:
+    """Yield start and end integer pairs from a complex range string like "1-9,12, 15-20,23".
+
+    >>> list(parse_range("1-9,12, 15-20,23"))
+    [(1, 9), (12, 12), (15, 20), (23, 23)]
+
+    >>> list(parse_range("1-9,12, 15-20,2-3-4"))
+    Traceback (most recent call last):
+        ...
+    ValueError: format error in 2-3-4
+    """
+    for x in expr.split(","):
+        elem = x.split("-")
+        if len(elem) == 1:  # a number
+            yield int(elem[0]), int(elem[0])
+        elif len(elem) == 2:  # a range inclusive
+            yield int(elem[0]), int(elem[1])
+        else:  # more than one hyphen
+            raise ValueError(f"format error in {x}")
 
 
 @click.command()
@@ -22,12 +44,23 @@ __version__ = "0.2.0"
 @click.option(
     "-d", "--dpi", default=100, show_default=True, help="DPI of generating images."
 )
-def main(filepath: str, token: str, dpi: int) -> None:
+@click.option("-p", "--pages", help="PDF pages to upload.")
+def main(filepath: str, token: str, dpi: int, pages: Optional[str]) -> None:
     """Upload PDF file to Gyazo as images then convert Scrapbox format."""
     client = gyazo.Api(access_token=token)
     urls = []
     with tempfile.TemporaryDirectory() as tempdir:
-        images: List[Image] = convert_from_path(filepath, dpi=dpi, fmt="png")
+        if pages is None:
+            images: List[Image] = convert_from_path(filepath, dpi=dpi, fmt="png")
+        else:
+            images: List[Image] = list(
+                chain.from_iterable(
+                    convert_from_path(
+                        filepath, dpi=dpi, fmt="png", first_page=first, last_page=last
+                    )
+                    for first, last in parse_range(pages)
+                )
+            )
         tempdir_p = Path(tempdir)
         for i, img in enumerate(tqdm(images, desc="Uploading")):
             img_path = tempdir_p / f"{i}.png"
--- tests/test_pdf2sb.py
+++ tests/test_pdf2sb.py
@@ -1,5 +1,18 @@
-from pdf2sb import __version__
+import pytest
+
+from pdf2sb import __version__, parse_range
 
 
-def test_version():
+def test_version() -> None:
     assert __version__ == "0.2.0"
+
+
+def test_parse_range() -> None:
+    assert list(parse_range("1-9,12, 15-20,23")) == [
+        (1, 9),
+        (12, 12),
+        (15, 20),
+        (23, 23),
+    ]
+    with pytest.raises(ValueError):
+        list(parse_range("1-9,12, 15-20,2-3-4"))
