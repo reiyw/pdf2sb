119acc922fd6177ed9c61a1ac569b7320774fcdc
reiyw <reiyw.setuve@gmail.com>
2019-02-08 15:09:29 +0900

    CLI interface

--- pdf2sb/__init__.py
+++ pdf2sb/__init__.py
@@ -1,22 +1,33 @@
-import os
-import sys
 import tempfile
 from pathlib import Path
 from typing import List
 
+import click
 import gyazo
 from PIL.Image import Image
 from pdf2image import convert_from_path
 from tqdm import tqdm
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
-def main() -> None:
-    client = gyazo.Api(access_token=os.getenv("GYAZO_ACCESS_TOKEN"))
+@click.command()
+@click.argument("filepath", type=click.Path(exists=True))
+@click.option(
+    "-t",
+    "--token",
+    envvar="GYAZO_ACCESS_TOKEN",
+    help="Access token for Gyazo. If not specified, use $GYAZO_ACCESS_TOKEN.",
+)
+@click.option(
+    "-d", "--dpi", default=100, show_default=True, help="DPI of generating images."
+)
+def main(filepath: str, token: str, dpi: int) -> None:
+    """Upload PDF file to Gyazo as images then convert Scrapbox format."""
+    client = gyazo.Api(access_token=token)
     urls = []
     with tempfile.TemporaryDirectory() as tempdir:
-        images: List[Image] = convert_from_path(sys.argv[1], dpi=100, fmt="png")
+        images: List[Image] = convert_from_path(filepath, dpi=dpi, fmt="png")
         tempdir_p = Path(tempdir)
         for i, img in enumerate(tqdm(images, desc="Uploading")):
             img_path = tempdir_p / f"{i}.png"
