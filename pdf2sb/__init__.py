2b7a0c8d13f9dba09eb78d6f1c9c02374728f1b2
reiyw <reiyw.setuve@gmail.com>
2019-07-10 21:39:56 +0900

    Add -l/--extract-links option

--- pdf2sb/__init__.py
+++ pdf2sb/__init__.py
@@ -3,12 +3,13 @@ from itertools import chain
 from pathlib import Path
 from typing import List, Iterator, Optional
 
+import PyPDF2
 import click
 import gyazo
 from PIL.Image import Image
 from pdf2image import convert_from_path
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 
 def parse_range(expr: str) -> Iterator[int]:
@@ -32,6 +33,53 @@ def parse_range(expr: str) -> Iterator[int]:
             raise ValueError(f"format error in {x}")
 
 
+def extract_links_from_pdf(
+    pdf_file: str, pages: Optional[List[int]] = None
+) -> Iterator[List[str]]:
+    # Mostly from https://stackoverflow.com/a/56299671
+    pdf = PyPDF2.PdfFileReader(open(pdf_file, "rb"))
+    annots = "/Annots"
+    uri = "/URI"
+    ank = "/A"
+    for page in range(pdf.getNumPages()):
+        if pages is not None and page not in pages:
+            continue
+
+        links = []
+        pageobj = pdf.getPage(page).getObject()
+        if annots in pageobj.keys():
+            ann = pageobj[annots]
+            for a in ann:
+                u = a.getObject()
+                if uri in u[ank].keys():
+                    links.append(u[ank][uri])
+        yield links
+
+
+def build_scrapbox_repr(
+    gyazo_urls: List[str],
+    expand: bool,
+    n_spaces: int,
+    links: Optional[List[List[str]]] = None,
+) -> str:
+    if links is None:
+        links = [[]] * len(gyazo_urls)
+
+    blocks = []
+    for gyazo_url, links_per_page in zip(gyazo_urls, links):
+        block = []
+        if expand:
+            block.append(f"> [[{gyazo_url}]]\n")
+        else:
+            block.append(f"> [{gyazo_url}]\n")
+        for link in links_per_page:
+            block.append(f" {link}\n")
+        block.append("\n" * n_spaces)
+        blocks.append("".join(block))
+
+    return "".join(blocks)
+
+
 def pdf2sb(
     pdf_file: str,
     gyazo_access_token: str,
@@ -39,6 +87,7 @@ def pdf2sb(
     n_spaces: int = 1,
     expand: bool = False,
     pages: Optional[str] = None,
+    extract_links: bool = False,
 ) -> str:
     """Upload PDF file to Gyazo as images then convert to Scrapbox format."""
     client = gyazo.Api(access_token=gyazo_access_token)
@@ -62,10 +111,21 @@ def pdf2sb(
                 img.save(img_path)
                 gyazoimg = client.upload_image(img_path.open("rb"))
                 urls.append(gyazoimg.to_dict()["permalink_url"])
-    if expand:
-        return "".join(f"> [[{url}]]\n" + "\n" * n_spaces for url in urls)
-    else:
-        return "".join(f"> [{url}]\n" + "\n" * n_spaces for url in urls)
+
+    if pages is not None:
+        # "1-4,6" --> [1, 2, 3, 4, 6]
+        pages = list(
+            chain.from_iterable(
+                range(start, end + 1) for start, end in parse_range(pages)
+            )
+        )
+
+    links = extract_links_from_pdf(pdf_file, pages) if extract_links else None
+
+    sb_repr = build_scrapbox_repr(
+        gyazo_urls=urls, expand=expand, n_spaces=n_spaces, links=links
+    )
+    return sb_repr
 
 
 @click.command()
@@ -88,8 +148,15 @@ def pdf2sb(
 )
 @click.option("-p", "--pages", help="PDF pages to upload.")
 @click.option("-e", "--expand", is_flag=True)
+@click.option("-l", "--extract-links", is_flag=True)
 def main(
-    filepath: str, token: str, dpi: int, spaces: int, pages: Optional[str], expand: bool
+    filepath: str,
+    token: str,
+    dpi: int,
+    spaces: int,
+    pages: Optional[str],
+    expand: bool,
+    extract_links: bool,
 ) -> None:
     click.echo(
         pdf2sb(
@@ -99,6 +166,7 @@ def main(
             n_spaces=spaces,
             expand=expand,
             pages=pages,
+            extract_links=extract_links,
         ),
         nl=False,
     )
